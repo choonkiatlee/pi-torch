6d551bf68f1067ab41fe44fd159f394dcb246196
Ubuntu <insightface@insightfacesmall.amwdgssys2xufktx1pz14oeamc.bx.internal.cloudapp.net>
2020-04-19 19:09:02 +0000

    update

--- install_pytorch.sh
+++ install_pytorch.sh
@@ -10,9 +10,9 @@ export MAX_JOBS=2 # For Github host
 export USE_CFLAGS=1
 export CFLAGS="-mfpu=neon -D__NEON__"
 
-apt-get update && apt-get install -y python3-cffi python3-numpy
+apt-get update && apt-get install -y python3-cffi python3-numpy libatlas-dev
 
-pip3 install cython wheel
+pip3 install cython wheel pyyaml pillow
 
 git clone --recursive https://github.com/pytorch/pytorch
 
